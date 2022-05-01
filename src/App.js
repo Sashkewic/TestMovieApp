e5f1523778e4fb5c3069f65e523e8fe0068406be
Sashkewic <sashkewicha@gmail.com>
2022-05-01 14:45:08 +0600

    What is implemented in the react-application: searching for movies, displaying movies, description for the movie outside of visiting the page with a description

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Movie } from "./components/Movie";
+
+import "./App.css";
+import Logo from "./images/movie-logo.png";
+import { MagnifyingGlass } from "phosphor-react";
+
+const POPULAR_FILMS_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=934aadbf9bcaca27b105d27884960f54";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=934aadbf9bcaca27b105d27884960f54&query="
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	useEffect(() => {
+		getMovies(POPULAR_FILMS_API);
+	}, []);
+
+	const getMovies = (API) => {
+		fetch(API).then(res => res.json()).then(data => {
+			setMovies(data.results);
+		});
+	}
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+
+		if (searchTerm) {
+			getMovies(SEARCH_API + searchTerm)
+			setSearchTerm('');
+		}
+	}
+
+	const handleOnChange = (e) => {
+		setSearchTerm(e.target.value);
+	}
+
+	return (
+		<div>
+			<header>
+				<div className="logo">
+					<img src={Logo} alt="" /> React Films
+				</div>
+
+				<form onSubmit={handleOnSubmit}>
+					<input type="text" placeholder="Search" className="search" value={searchTerm} onChange={handleOnChange} />
+					<button type="submit"><MagnifyingGlass size={22} color="#FFF" /></button>
+				</form>
+			</header>
+
+			<div className="movie-container">
+				{movies.length > 0 && movies.map(movie => (
+					< Movie key={movie.id} {...movie} />
+				))}
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+
+const setVote = (vote) => {
+    if (vote >= 8) {
+        return 'green'
+    }
+
+    else if (vote >= 5) {
+        return 'orange'
+    }
+
+    else {
+        return 'red'
+    }
+}
+
+export const Movie = ({ title, poster_path, overview, vote_average, release_date, favorite = false }) => {
+
+    // if (favorite) {
+    //     color += "red";
+    // }
+
+    return (
+        <div className="movie">
+            <div className="movie-header">
+                <img src={poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80'} alt={title} />
+            </div>
+
+            <div className="movie-info">
+                <h3 className="movie-name">{title}</h3>
+                <span className={`tag ${setVote(vote_average)}`}>{vote_average}</span>
+            </div>
+
+            <div className="movie-over">
+                <span>Date: {release_date}</span>
+                <h2>Overview</h2>
+                <p>{overview}</p>
+            </div>
+        </div>
+    )
+}
